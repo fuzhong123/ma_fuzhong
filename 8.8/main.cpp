abff3e88371daafd3103ef2311023e61cd3f78f0
DESKTOP-90UJ7UG\Administrator <863242240@qq.com>
2018-12-28 16:31:15 +0800

    第八章作业 马福忠 测绘工程2017-3 201701020319

---
+++ 8.8/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //)a
+    unsigned int values[size]={2,4,6,8,10};
+
+    //)b
+    unsigned int *vPer;
+
+    //)c
+    for(int i=0;i<size;i++)
+    {
+        cout<<values[i]<<endl;
+
+    }
+
+    //)d
+    vPer=values;
+    vPer=values[0];
+
+    //)e
+    for(int i=0;i<size;i++)
+    {
+        cout<<vPer+i<<endl;
+    }
+
+    //)f
+    for(int i=0;i<size;i++)
+    {
+        cout<<values+i<<endl;
+    }
+
+    //g
+    for (int i=0;i<size;i++)
+    {
+        cout<<vPer[i]<<endl;
+    }
+
+    //h
+    values[4]
+    *(values+4)
+    *(vPer+4)
+    vper[4]
+
+    //i
+    1002500+4*3=1002512
+    8
+
+    //j
+    1002500
+    2
+    return 0;
+}
---
+++ 8.9/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long*longPtr;
+    longPtr=&value1;
+    cout<<*longPtr;
+    alue2*longPtr;
+    cout<<&value2;
+    cout<<longPtr;
+
+
+
+    Ò»Ñש
+    return 0;
+}
