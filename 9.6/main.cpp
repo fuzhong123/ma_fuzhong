c55f727d730f18fc5c21de07be83f22f54a5d878
DESKTOP-90UJ7UG\Administrator <863242240@qq.com>
2018-12-28 21:32:24 +0800

    第九章作业 马福忠 测绘工程2017-3 201701020319

---
+++ 9.6/main.cpp
@@ -0,0 +1,91 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+class Rational
+{
+public:
+   Rational( int = 0, int = 1 );
+   Rational addition( const Rational & );
+   Rational subtraction( const Rational & );
+   Rational multiplication( const Rational & );
+   Rational division( const Rational & );
+   void printRational ();
+   void printRationalAsDouble();
+private:
+   int numerator;
+   int denominator;
+   void reduction();
+};
+#endif
+
+#include <iostream>
+using std::cout;
+
+Rational::Rational( int n, int d )
+{
+   numerator = n;
+   denominator = d;
+   reduction();
+}
+Rational Rational::addition( const Rational &a )
+{
+   Rational t;
+   t.numerator = a.numerator * denominator;
+   t.numerator += a.denominator * numerator;
+   t.denominator = a.denominator * denominator;
+   t.reduction();
+   return t;
+}
+Rational Rational::subtraction( const Rational &s )
+{
+   Rational t;
+   t.numerator = s.denominator * numerator;
+   t.numerator -= denominator * s.numerator;
+   t.denominator = s.denominator * denominator;
+   t.reduction();
+   return t;
+}
+Rational Rational::multiplication( const Rational &m )
+{
+   Rational t;
+   t.numerator = m.numerator * numerator;
+   t.denominator = m.denominator * denominator;
+   t.reduction();
+   return t;
+}
+Rational Rational::division( const Rational &v )
+{
+   Rational t;
+   t.numerator = v.denominator * numerator;
+   t.denominator = denominator * v.numerator;
+   t.reduction();
+   return t;
+}
+void Rational::printRational ()
+{
+   if ( denominator == 0 )
+      cout << "\nDIVIDE BY ZERO ERROR!!!" << '\n';
+   else if ( numerator == 0 )
+      cout << 0;
+   else
+      cout << numerator << '/' << denominator;
+}
+void Rational::printRationalAsDouble()
+{
+   cout << static_cast< double >( numerator ) / denominator;
+}
+void Rational::reduction()
+{
+   int largest;
+   largest = numerator > denominator ? numerator : denominator;
+   int gcd = 0;
+   for ( int loop = 2; loop <= largest; loop++ )
+
+       if ( numerator % loop == 0 && denominator % loop == 0 )
+          gcd = loop;
+   if (gcd != 0)
+   {
+      numerator /= gcd;
+      denominator /= gcd;
+   }
+}
